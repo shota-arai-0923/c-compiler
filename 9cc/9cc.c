aa6458de6e8d313c9e922f939eb477f5f818737f
Shota Arai <shotashota00923@gmail.com>
2025-01-01 10:50:10 +0000

    add and minus

--- 9cc/9cc.c
+++ 9cc/9cc.c
@@ -7,11 +7,32 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  char *p = argv[1];
+
   printf(".text\n");
   printf(".global main\n");
   printf("main:\n");
-  printf("  mov x0, #%d\n", atoi(argv[1])); // x0に引数の値をロード
+  printf("  mov x0, #%ld\n", strtol(p, &p, 10)); // 最初の数値をx0に格納
+
+  while (*p) {
+    if (*p == '+') {
+      p++;
+      printf("  add x0, x0, #%ld\n", strtol(p, &p, 10)); // 加算
+      continue;
+    }
+
+    if (*p == '-') {
+      p++;
+      printf("  sub x0, x0, #%ld\n", strtol(p, &p, 10)); // 減算
+      continue;
+    }
+
+    fprintf(stderr, "予期しない文字です: '%c'\n", *p);
+    return 1;
+  }
+
   printf("  ret\n");
   return 0;
 }
+
 
